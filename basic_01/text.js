49152e47993899e812a4f31f19cfce976e59d115
Bayezidemon <136912707+Bayezidemon@users.noreply.github.com>
2023-12-09 19:15:14 +0000

    programming

--- basic_01/text.js
+++ basic_01/text.js
@@ -1,1 +1,15 @@
 console.log("Bayezid Hossain")
+
+let acountName = 2120109;
+let MyName = "Bayezid";
+let myEmail = "mdbayezidemon@gmail.com";
+let acountState;
+
+let score = "17abdevillias";
+console.log(typeof score);
+
+console.table([acountName, MyName, myEmail, acountState]);
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
