1becd51bd2b95cc740500c35163178cc7ca1dcba
Bayezidemon <136912707+Bayezidemon@users.noreply.github.com>
2023-12-10 05:45:27 +0000

    programming

--- basic_01/text.js
+++ basic_01/text.js
@@ -21,10 +21,22 @@ const name = "Bayezid Hossain";
 const repoCount = 50;
 //console.log(`My name is ${name} and my repo count is ${repoCount}`);
 
-console.log(Math.random());
-console.log((Math.random()*10) +1);
-console.log(Math.floor(Math.random()*10) +1);
+// console.log(Math.random());
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor(Math.random()*10) +1);
 
 const min = 10;
 const max = 20;
-console.log(Math.floor(Math.random() * (max-min +1) + min));
+//console.log(Math.floor(Math.random() * (max-min +1) + min));
+
+let myDate = new Date();
+//console.log(myDate,toString());
+
+let createDate = new Date(2023, 11, 10);
+// console.log(createDate.toDateString());
+
+let myCreateDate = new Date("2023-12-10");
+//console.log(myCreateDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+console.log(myCreateDate.getDate());
